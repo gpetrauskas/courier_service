de7afdd4ace9792a666d341bb5f15b4ce4455a1d
gpetrauskas <inconoir@gmail.com>
2024-02-17 18:58:43 +0200

    feat: Add UserDTO record for the user data transfer
    Added UserDTO record to facilitate data transfer for user related operations

--- src/main/java/com/example/courier/dto/UserDTO.java
+++ src/main/java/com/example/courier/dto/UserDTO.java
@@ -1,4 +1,13 @@
 package com.example.courier.dto;
 
-public record UserDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserDTO(
+        @NotBlank String name,
+        @NotBlank @Email String email,
+        @NotBlank String address,
+        @NotBlank @Size(min = 8, max = 16, message = "Password must be between 8-16 characters.") String password
+) {
 }
