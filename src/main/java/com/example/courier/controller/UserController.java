b5d32f0d6fe24ccfe596e24e920e08372f77acda
gpetrauskas <inconoir@gmail.com>
2024-02-18 21:53:47 +0200

    feat: Add UserController logic for handling user registration requests

--- src/main/java/com/example/courier/controller/UserController.java
+++ src/main/java/com/example/courier/controller/UserController.java
@@ -3,7 +3,10 @@ package com.example.courier.controller;
 import com.example.courier.dto.UserDTO;
 import com.example.courier.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody UserDTO userDTO) {
         try {
+            logger.info("Received a registration request for user: {}", userDTO.email());
             userService.registerUser(userDTO);
+            return ResponseEntity.ok("User registered successfully.");
         } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Problem occurred during registration");
         }
-        return null;
     }
 
 }
