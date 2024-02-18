30e645c9afe95af68af133557eb1f9f7f308849a
gpetrauskas <inconoir@gmail.com>
2024-02-18 21:56:51 +0200

    feat: Add SecurityConfig class
    Initial security filter chain configuration

--- src/main/java/com/example/courier/config/SecurityConfig.java
+++ src/main/java/com/example/courier/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.example.courier.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/api/user/register")
+                                .permitAll()
+                                .anyRequest().authenticated());
+        return httpSecurity.build();
+    }
 }
