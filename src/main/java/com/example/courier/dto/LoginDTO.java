5fba97542a6dd47f1198f1f8cd61fce221d90848
gpetrauskas <inconoir@gmail.com>
2024-02-19 19:36:08 +0200

    Added LoginDTO class for handling login request data.

--- src/main/java/com/example/courier/dto/LoginDTO.java
+++ src/main/java/com/example/courier/dto/LoginDTO.java
@@ -1,4 +1,10 @@
 package com.example.courier.dto;
 
-public record Login() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginDTO(
+        @Email @NotBlank String email,
+        @NotBlank @Size(min = 8, max = 16) String password) {
 }
