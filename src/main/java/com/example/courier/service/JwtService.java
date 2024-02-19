ae80c6f6d3382c105405b9afb56d317617bf9f24
gpetrauskas <inconoir@gmail.com>
2024-02-19 23:30:25 +0200

    feat: Implemented initial logic to generate, validate token. Added method to generate secret key.

--- src/main/java/com/example/courier/service/JwtService.java
+++ src/main/java/com/example/courier/service/JwtService.java
@@ -1,4 +1,38 @@
 package com.example.courier.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    private SecretKey secretKey = generateKey();
+
+    public String createToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + 3600000))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String validateToken(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    private SecretKey generateKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSha256");
+            return keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
