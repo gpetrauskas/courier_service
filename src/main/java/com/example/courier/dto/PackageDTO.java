daac3d46947195a56183be950bea23e342c58395
gpetrauskas <inconoir@gmail.com>
2024-02-20 16:11:07 +0200

    Added PackageDTO

--- src/main/java/com/example/courier/dto/PackageDTO.java
+++ src/main/java/com/example/courier/dto/PackageDTO.java
@@ -1,4 +1,5 @@
 package com.example.courier.dto;
 
-public record PackageDTO() {
+public record PackageDTO(Long id, double weight, String dimensions, String contents,
+                         String trackingNumber, String status) {
 }
