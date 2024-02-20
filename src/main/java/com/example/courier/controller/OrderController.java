eebbec3a8bd651661607a818e0ef3a17fea4cd1e
gpetrauskas <inconoir@gmail.com>
2024-02-20 23:46:59 +0200

    feat: Implemented getAllOrders and placeOrder endpoints to retrieve all orders made by user and to place a new one.
    Add logic to matToOrderDTO method.

--- src/main/java/com/example/courier/controller/OrderController.java
+++ src/main/java/com/example/courier/controller/OrderController.java
@@ -1,24 +1,73 @@
 package com.example.courier.controller;
 
+import com.example.courier.domain.Order;
+import com.example.courier.domain.User;
 import com.example.courier.dto.OrderDTO;
+import com.example.courier.repository.OrderRepository;
+import com.example.courier.repository.UserRepository;
+import com.example.courier.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+
     @GetMapping("/getAllOrders")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<OrderDTO>> getOrders() {
-        List<OrderDTO> orderDTOs = null;
-
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByEmail(auth.getName());
+        List<Order> orders = orderRepository.findByUserId(user.getId());
+        List<OrderDTO> orderDTOs = orders.stream()
+                .map(order -> mapToOrderDTO(order))
+                .collect(Collectors.toList());
         return ResponseEntity.ok(orderDTOs);
+    }
+
+    private OrderDTO mapToOrderDTO(Order order) {
+        OrderDTO orderDTO = new OrderDTO(order.getUser().getId(),
+                order.getSenderAddress(),
+                order.getRecipientAddress(),
+                order.getPackageDetails(),
+                order.getDeliveryPreferences(),
+                order.getStatus(),
+                order.getCreateDate());
+
+        return orderDTO;
+    }
+
+    @PostMapping("/placeOrder")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> addOrder(@RequestBody OrderDTO orderDTO) {
+
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            User user = userRepository.findByEmail(auth.getName());
+
+            BigDecimal shippingCost = orderService.calculateShippingCost(orderDTO);
+
+            orderService.placeOrder(user.getId(), orderDTO, shippingCost);
+            return ResponseEntity.ok("Order placed successfully. Shipping cost: " + shippingCost);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Problem occurred placing order.");
+        }
     }
 
 }
