7e6879300340efbed778c75d8d94e229ce20cb0e
gpetrauskas <inconoir@gmail.com>
2024-02-20 16:12:59 +0200

    Initial implementation for logic related to orders

--- src/main/java/com/example/courier/service/OrderService.java
+++ src/main/java/com/example/courier/service/OrderService.java
@@ -1,4 +1,26 @@
 package com.example.courier.service;
 
+import com.example.courier.dto.OrderDTO;
+import com.example.courier.repository.OrderRepository;
+import com.example.courier.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public void placeOrder(OrderDTO orderDTO) {
+
+    }
+
+    public BigDecimal calculateShippingCost(OrderDTO orderDTO) {
+        return null;
+    }
 }
