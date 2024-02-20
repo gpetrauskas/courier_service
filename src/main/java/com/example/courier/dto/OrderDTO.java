93f4b0258f35577bf97f4b834f55d5ee44f0189d
gpetrauskas <inconoir@gmail.com>
2024-02-20 23:51:44 +0200

    Added constructor to OrderDTO

--- src/main/java/com/example/courier/dto/OrderDTO.java
+++ src/main/java/com/example/courier/dto/OrderDTO.java
@@ -1,7 +1,9 @@
 package com.example.courier.dto;
 
+import com.example.courier.domain.Package;
+
 import java.time.LocalDateTime;
 
-public record OrderDTO(Long id, String senderAddress, String recipientAddress, PackageDTO packageDetails,
+public record OrderDTO(Long id, String senderAddress, String recipientAddress, Package packageDetails,
                        String deliveryPreferences, String status, LocalDateTime createTime) {
 }
