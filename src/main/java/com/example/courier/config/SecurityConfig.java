db69612de907bc871922fc86a9f5f3a597614f15
gpetrauskas <inconoir@gmail.com>
2024-02-20 12:14:04 +0200

    Added JwtAuthenticationFilter to the filter chain to handle JWT authentication before UsernamePasswordAuthenticationFilter

--- src/main/java/com/example/courier/config/SecurityConfig.java
+++ src/main/java/com/example/courier/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.example.courier.config;
 
+import com.example.courier.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtService jwtService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -15,7 +21,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/api/user/register", "/api/user/login").permitAll()
-                                .anyRequest().authenticated());
+                                .anyRequest().authenticated())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtService),
+                        UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 }
