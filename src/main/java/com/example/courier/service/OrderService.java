f6d40b54da08eafa790412781367cc4739be0e90
gpetrauskas <inconoir@gmail.com>
2024-02-21 00:04:35 +0200

    feat: Implemented initial working placeOrder method to create a new order and Package details with it.
    feat: Added initial working calculateShippingCost method. Calculates a shipping price based of weight and delivery preferences.

--- src/main/java/com/example/courier/service/OrderService.java
+++ src/main/java/com/example/courier/service/OrderService.java
@@ -1,12 +1,18 @@
 package com.example.courier.service;
 
+import com.example.courier.domain.Order;
+import com.example.courier.domain.Package;
+import com.example.courier.domain.User;
 import com.example.courier.dto.OrderDTO;
 import com.example.courier.repository.OrderRepository;
+import com.example.courier.repository.PackageRepository;
 import com.example.courier.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class OrderService {
@@ -15,12 +21,42 @@ public class OrderService {
     private OrderRepository orderRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PackageRepository packageRepository;
 
-    public void placeOrder(OrderDTO orderDTO) {
+    public void placeOrder(Long id, OrderDTO orderDTO, BigDecimal shippingPrice) {
+        // fetch user  from the database
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found."));
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setSenderAddress(orderDTO.senderAddress());
+        order.setRecipientAddress(orderDTO.recipientAddress());
+        order.setDeliveryPreferences(orderDTO.deliveryPreferences());
+        order.setStatus("Pending");
+        order.setCreateDate(LocalDateTime.now());
+
+        Package packageDetails = new Package();
+        packageDetails.setWeight(orderDTO.packageDetails().getWeight());
+        packageDetails.setDimensions(orderDTO.packageDetails().getDimensions());
+        packageDetails.setContents(orderDTO.packageDetails().getContents());
+        packageDetails.setTrackingNumber(UUID.randomUUID().toString());
+        packageDetails.setStatus("Not Shipped");
+
+        order.setPackageDetails(packageDetails);
+
+        orderRepository.save(order);
 
     }
 
     public BigDecimal calculateShippingCost(OrderDTO orderDTO) {
-        return null;
+        BigDecimal shippingCost = new BigDecimal(0);
+        if (orderDTO.deliveryPreferences().equals("express")) {
+            shippingCost = shippingCost.add(new BigDecimal(10));
+        }
+        if (orderDTO.packageDetails().getWeight() > 10) {
+            shippingCost.add(new BigDecimal(5));
+        }
+        return shippingCost;
     }
 }
