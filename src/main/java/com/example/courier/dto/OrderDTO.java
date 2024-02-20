177a5c16dea8e992ab927f332c966750741da915
gpetrauskas <inconoir@gmail.com>
2024-02-20 16:10:26 +0200

    Added OrderDTO

--- src/main/java/com/example/courier/dto/OrderDTO.java
+++ src/main/java/com/example/courier/dto/OrderDTO.java
@@ -1,4 +1,7 @@
 package com.example.courier.dto;
 
-public record OrderDTO() {
+import java.time.LocalDateTime;
+
+public record OrderDTO(Long id, String senderAddress, String recipientAddress, PackageDTO packageDetails,
+                       String deliveryPreferences, String status, LocalDateTime createTime) {
 }
