12914a86f91e4dba894537d55c187063bfd66b04
gpetrauskas <inconoir@gmail.com>
2024-02-20 23:54:42 +0200

    refactor: findByUserId to return List of Order instead of Order
    Add findByTrackingNumber method to find the Order

--- src/main/java/com/example/courier/repository/OrderRepository.java
+++ src/main/java/com/example/courier/repository/OrderRepository.java
@@ -3,6 +3,9 @@ package com.example.courier.repository;
 import com.example.courier.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Order findByUserId(Long userId);
+    List<Order> findByUserId(Long userId);
+    Order findByTrackingNumber(String trackingNumber);
 }
