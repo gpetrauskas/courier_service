2025e7661b0541fbad512bf5d616c058a2a70930
gpetrauskas <inconoir@gmail.com>
2024-02-22 00:51:43 +0200

    refactor: Change return type of findByTrackingNumber to Optional

--- src/main/java/com/example/courier/repository/PackageRepository.java
+++ src/main/java/com/example/courier/repository/PackageRepository.java
@@ -3,5 +3,8 @@ package com.example.courier.repository;
 import com.example.courier.domain.Package;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PackageRepository extends JpaRepository<Package, Long> {
+    Optional<Package> findByTrackingNumber(String trackingNumber);
 }
