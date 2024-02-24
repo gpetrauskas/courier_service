b6e603143a5f5f5a6fec8693c97379f323b1b034
gpetrauskas <inconoir@gmail.com>
2024-02-24 23:32:01 +0200

    Refactor validateToken method to return nested Map for authentication details.
    Added role claim to JWT token creation in createToken method.

--- src/main/java/com/example/courier/service/JwtService.java
+++ src/main/java/com/example/courier/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.example.courier.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
@@ -8,23 +9,34 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
 
     private SecretKey secretKey = generateKey();
 
-    public String createToken(String email) {
+    public String createToken(String email, String role) {
         return Jwts.builder()
                 .subject(email)
+                .claim("role", role)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + 3600000))
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String validateToken(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
+    public Map<String, String> validateToken(String token) {
+        Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+        String subject = claims.getSubject();
+        String role = claims.get("role", String.class);
+
+        Map<String, String> authDetails = new HashMap<>();
+        authDetails.put("subject", subject);
+        authDetails.put("role", role);
+
+        return authDetails;
     }
 
     private SecretKey generateKey() {
