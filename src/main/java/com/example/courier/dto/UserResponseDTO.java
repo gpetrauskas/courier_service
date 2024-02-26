86ae80641bd836817279bf6e0d13096b272c2499
gpetrauskas <inconoir@gmail.com>
2024-02-26 21:44:26 +0200

    Created a UserResponseDTO record with method fromUser that converts User entities to UserResponseDTO objects.

--- src/main/java/com/example/courier/dto/UserResponseDTO.java
+++ src/main/java/com/example/courier/dto/UserResponseDTO.java
@@ -1,4 +1,9 @@
 package com.example.courier.dto;
 
-public record UserResponseDTO() {
+import com.example.courier.domain.User;
+
+public record UserResponseDTO(String name, String email, String address) {
+    public static UserResponseDTO fromUser(User user) {
+        return new UserResponseDTO(user.getName(), user.getEmail(), user.getAddress());
+    }
 }
