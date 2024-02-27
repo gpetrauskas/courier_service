ce90438c8240a2d268eca8914d74b978e7ff5535
gpetrauskas <inconoir@gmail.com>
2024-02-27 12:36:23 +0200

    Added ValidationException to throw exception for email pattern mismatch. Added more logging for testing purposes.

--- src/main/java/com/example/courier/service/UserService.java
+++ src/main/java/com/example/courier/service/UserService.java
@@ -33,20 +33,25 @@ public class UserService {
             User newUser = createUserFromDTO(userDTO);
             logger.info("before saving user");
             userRepository.save(newUser);
+            logger.info("user saved");
         } catch (RuntimeException e) {
-            System.out.println("Error during registration: User already registered with email: " + userDTO.email());
+            logger.info("Error during registration: {}", e.getMessage());
             throw e;
         }
     }
 
     private void validateUserRegistration(UserDTO userDTO) {
         User user = userRepository.findByEmail(userDTO.email());
+        logger.info("Validating user: {}", userDTO.email());
         if (userDTO.password().length() < 8 || userDTO.password().length() > 16) {
             logger.info("Password length");
             throw new ValidationException("Password length must be between 8-16 characters.");
         } else if (user != null) {
             logger.info("User not null");
             throw new RuntimeException("User already exist.");
+        } else if (!userDTO.email().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            logger.info("email validation");
+            throw new ValidationException("Email is not valid.");
         }
     }
 
