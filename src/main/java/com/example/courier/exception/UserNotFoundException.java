e0d83a546097e31b1ca1f467c8666d1aac6775cd
gpetrauskas <inconoir@gmail.com>
2024-02-27 12:32:07 +0200

    refactor: change return type of findById to Optional.

---
+++ src/main/java/com/example/courier/exception/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.courier.exception;
+
+public class UserNotFoundException {
+}
--- src/main/java/com/example/courier/repository/UserRepository.java
+++ src/main/java/com/example/courier/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.courier.repository;
 import com.example.courier.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    Optional<User> findById(Long id);
 }
