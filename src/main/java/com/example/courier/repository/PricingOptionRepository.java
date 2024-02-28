4608c2fde5c15b1ab69765a8239bdb32c0ea5963
gpetrauskas <inconoir@gmail.com>
2024-02-28 19:20:34 +0200

    Added repository for pricing options.

--- src/main/java/com/example/courier/repository/PricingOptionRepository.java
+++ src/main/java/com/example/courier/repository/PricingOptionRepository.java
@@ -1,4 +1,8 @@
 package com.example.courier.repository;
 
-public interface PricingOptionRepository {
+import com.example.courier.domain.PricingOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PricingOptionRepository extends JpaRepository<PricingOption, Long> {
+    PricingOption findByName(String name);
 }
