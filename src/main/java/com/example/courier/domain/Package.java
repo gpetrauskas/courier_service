60b3ca73fe19e200d3a25280f33e8a0468ff4408
gpetrauskas <inconoir@gmail.com>
2024-02-28 17:43:04 +0200

    Added getter and setter for id

--- src/main/java/com/example/courier/domain/Package.java
+++ src/main/java/com/example/courier/domain/Package.java
@@ -74,4 +74,12 @@ public class Package {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
