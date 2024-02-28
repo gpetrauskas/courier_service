9b6d049ed09f4e89aee529c64771aabe9827ecea
gpetrauskas <inconoir@gmail.com>
2024-02-28 19:19:54 +0200

    Added ProcingOptio entity.
    This class represent pricing for shipping services.

---
+++ src/main/java/com/example/courier/domain/PricingOption.java
@@ -0,0 +1,60 @@
+package com.example.courier.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pricing_options")
+public class PricingOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    @NotBlank
+    private String name;
+    @Column
+    @NotBlank
+    private String description;
+    @Column
+    @NotBlank
+    private BigDecimal price;
+
+    public PricingOption() {}
+
+    public PricingOption(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
