fada117328657126c02133c3e311cec70202890d
gpetrauskas <inconoir@gmail.com>
2024-02-28 22:59:55 +0200

    Added method to receive all pricing options.

--- src/main/java/com/example/courier/service/PricingOptionService.java
+++ src/main/java/com/example/courier/service/PricingOptionService.java
@@ -1,4 +1,19 @@
 package com.example.courier.service;
 
+import com.example.courier.domain.PricingOption;
+import com.example.courier.repository.PricingOptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PricingOptionService {
+
+    @Autowired
+    private PricingOptionRepository pricingOptionRepository;
+
+    public List<PricingOption> getAllPricingOptions() {
+        return pricingOptionRepository.findAll();
+    }
 }
