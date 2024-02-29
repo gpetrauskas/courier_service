2e3ce9e5fb1b0c187bf52a151fcdd2437f5b9d7c
gpetrauskas <inconoir@gmail.com>
2024-02-29 23:35:46 +0200

    refactor: to return Optionl<PriceOption>

--- src/main/java/com/example/courier/repository/PricingOptionRepository.java
+++ src/main/java/com/example/courier/repository/PricingOptionRepository.java
@@ -3,6 +3,8 @@ package com.example.courier.repository;
 import com.example.courier.domain.PricingOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PricingOptionRepository extends JpaRepository<PricingOption, Long> {
-    PricingOption findByName(String name);
+    Optional<PricingOption> findByName(String name);
 }
