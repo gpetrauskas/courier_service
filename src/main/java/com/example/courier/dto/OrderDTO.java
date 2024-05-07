73776a132ea418aafce45abee4fef33296546226
gpetrauskas <inconoir@gmail.com>
2024-05-07 18:52:09 +0300

    Added date formatter to respond with more user friendly format

--- src/main/java/com/example/courier/dto/OrderDTO.java
+++ src/main/java/com/example/courier/dto/OrderDTO.java
@@ -3,7 +3,13 @@ package com.example.courier.dto;
 import com.example.courier.domain.Package;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record OrderDTO(Long id, String senderAddress, String recipientAddress, Package packageDetails,
                        String deliveryPreferences, String status, LocalDateTime createTime) {
+
+    public String getCreateTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return this.createTime.format(formatter);
+    }
 }
