5cd0b01608df1c329974c96af3bc4b98addbc0f8
gpetrauskas <inconoir@gmail.com>
2024-05-14 23:20:29 +0300

    Update User entity with PaymentMethod and initialize list in constrctor.

--- src/main/java/com/example/courier/domain/User.java
+++ src/main/java/com/example/courier/domain/User.java
@@ -6,6 +6,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -34,7 +37,12 @@ public class User {
     @Column
     private Role role;
 
-    public User() {}
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<PaymentMethod> paymentMethods;
+
+    public User() {
+        this.paymentMethods = new ArrayList<>();
+    }
 
     public User(String name, String email, String address, String password) {
         this.name = name;
