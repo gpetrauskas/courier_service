a47a0b0a393c9e46a6ef70b6dd47e470f17ae8d7
gpetrauskas <inconoir@gmail.com>
2024-05-14 23:17:46 +0300

    Create abstract PaymentMethod entity.

--- src/main/java/com/example/courier/domain/PaymentMethod.java
+++ src/main/java/com/example/courier/domain/PaymentMethod.java
@@ -1,4 +1,25 @@
 package com.example.courier.domain;
 
-public class PaymentMethod {
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "payment_type")
+public abstract class PaymentMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
 }
