ff360c619a6e9768b14604bc1322974066fc2bb9
gpetrauskas <inconoir@gmail.com>
2024-05-16 00:33:50 +0300

    feat: add more fields related with PaymentMethod to PaymentDTO.

--- src/main/java/com/example/courier/dto/PaymentDTO.java
+++ src/main/java/com/example/courier/dto/PaymentDTO.java
@@ -4,5 +4,6 @@ import com.example.courier.domain.Order;
 
 import java.math.BigDecimal;
 
-public record PaymentDTO(Order order, String paymentMethod, BigDecimal amount, String status) {
+public record PaymentDTO(Order order, Long paymentMethodId, PaymentMethodDTO newPaymentMethod,
+                         BigDecimal amount, String status) {
 }
