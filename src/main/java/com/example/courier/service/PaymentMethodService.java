37ea35381e7b1a1d11091f5fd7da2138fda5e01b
gpetrauskas <inconoir@gmail.com>
2024-05-16 00:55:09 +0300

    feat: add method to save a CreditCard payment method.
    Implemented method 'addPaymentMEthod'. It checks if the provided paymentMethodDTO instance of CreditCard. If so it creates and saves CC entity.

--- src/main/java/com/example/courier/service/PaymentMethodService.java
+++ src/main/java/com/example/courier/service/PaymentMethodService.java
@@ -1,13 +1,16 @@
 package com.example.courier.service;
 
+import com.example.courier.domain.CreditCard;
 import com.example.courier.domain.User;
+import com.example.courier.dto.CreditCardDTO;
+import com.example.courier.dto.PaymentMethodDTO;
 import com.example.courier.repository.PaymentMethodRepository;
 import com.example.courier.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PaymentServiceMethod {
+public class PaymentMethodService {
 
     @Autowired
     private UserRepository userRepository;
@@ -18,5 +21,16 @@ public class PaymentServiceMethod {
         User user = userRepository.findById(userId).orElseThrow(() ->
                 new RuntimeException("User not found."));
 
+        if (paymentMethodDTO instanceof CreditCardDTO) {
+            CreditCardDTO creditCardDTO = (CreditCardDTO) paymentMethodDTO;
+
+            CreditCard card = new CreditCard();
+            card.setUser(user);
+            card.setCardNumber(creditCardDTO.cardNumber());
+            card.setExpiryDate(creditCardDTO.expiryDate());
+            card.setCardHolderName(creditCardDTO.cardHolderName());
+
+            paymentMethodRepository.save(card);
+        }
     }
 }
