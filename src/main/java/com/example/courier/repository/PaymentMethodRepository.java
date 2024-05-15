7bb7294f853e02838b150ea80ecb8af1a54524b3
gpetrauskas <inconoir@gmail.com>
2024-05-16 00:49:24 +0300

    Add PaymentMethod repository

--- src/main/java/com/example/courier/repository/PaymentMethodRepository.java
+++ src/main/java/com/example/courier/repository/PaymentMethodRepository.java
@@ -1,7 +1,8 @@
 package com.example.courier.repository;
 
+import com.example.courier.domain.PaymentMethod;
 import com.example.courier.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentMethodRepository extends JpaRepository<User, Long> {
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
 }
