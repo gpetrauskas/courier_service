9f39d5ef2172f35d2de1e0da02f1133ef98f9f7b
gpetrauskas <inconoir@gmail.com>
2024-05-16 17:47:27 +0300

    feat: Add method to process payment. It checks if paymentDTO has newPaymentmethod empty or not. If so it checks instanceof CreditCard (currently only one i have). when create a new CC object and save it. otherwise it checks if paymentMethodId is not null, if not it takes saved CC data from database and checks if that card match current user.
    After payement it updates Order, Package and Payment status

--- src/main/java/com/example/courier/service/PaymentService.java
+++ src/main/java/com/example/courier/service/PaymentService.java
@@ -1,58 +1,82 @@
 package com.example.courier.service;
 
-import com.example.courier.domain.Order;
-import com.example.courier.domain.Payment;
-import com.example.courier.domain.User;
+import com.example.courier.domain.*;
+import com.example.courier.dto.CreditCardDTO;
 import com.example.courier.dto.PaymentDTO;
+import com.example.courier.dto.PaymentMethodDTO;
 import com.example.courier.repository.OrderRepository;
+import com.example.courier.repository.PaymentMethodRepository;
 import com.example.courier.repository.PaymentRepository;
 import com.example.courier.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PaymentService {
 
+    private static final Logger log = LoggerFactory.getLogger(PaymentService.class);
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private PaymentRepository paymentRepository;
+    @Autowired
+    private PaymentMethodRepository paymentMethodRepository;
 
-    public void processPayment(PaymentDTO paymentDTO) {
-        User user = userRepository.findById(paymentDTO.order().getUser().getId()).orElseThrow(() ->
-                new RuntimeException("User not found."));
-        Order order = orderRepository.findById(paymentDTO.order().getId()).orElseThrow(() ->
+    public ResponseEntity<?> processPayment(PaymentDTO paymentDTO, Payment payment) {
+        Order order = orderRepository.findById(payment.getOrder().getId()).orElseThrow(() ->
                 new RuntimeException("Order not found."));
 
-        Payment payment = new Payment();
-        payment.setOrder(order);
-        payment.setPaymentMethod(paymentDTO.paymentMethod());
-        payment.setAmount(paymentDTO.amount());
-        payment.setStatus("WAITING");
-
         try {
-            if (paymentDTO.paymentMethod().equals("credit_card")) {
-                /// logic
-                payment.setStatus("PAID");
-                order.setStatus("CONFIRMED");
-                order.getPackageDetails().setStatus("PICKING_UP");
-            } else if (paymentDTO.paymentMethod().equals("online_banking")) {
-                /// logic
-                payment.setStatus("PAID");
-                order.setStatus("CONFIRMED");
-                order.getPackageDetails().setStatus("PICKING_UP");
-            }
-        } catch (Exception e) {
-            // fail
-            payment.setStatus("FAILED");
-            order.setStatus("PAYMENT_FAILED");
-        }
+            System.out.println("1");
+            if (paymentDTO.newPaymentMethod() != null) {
+                PaymentMethodDTO paymentMethodDTO = paymentDTO.newPaymentMethod();
+                System.out.println("2");
+                if (paymentMethodDTO instanceof CreditCardDTO) {
+                    CreditCardDTO creditCard = (CreditCardDTO) paymentMethodDTO;
 
-        paymentRepository.save(payment);
-        orderRepository.save(order);
+                    CreditCard card = new CreditCard();
+                    card.setUser(payment.getOrder().getUser());
+                    card.setCardHolderName(creditCard.cardHolderName());
+                    card.setCardNumber(creditCard.cardNumber());
+                    card.setExpiryDate(creditCard.expiryDate());
+
+                    System.out.println("3");
+                    paymentMethodRepository.saveAndFlush(card);
+                    payment.setPaymentMethod(card);
+                }
+            } else if (paymentDTO.paymentMethodId() != null) {
+                PaymentMethod pm = paymentMethodRepository.findById(paymentDTO.paymentMethodId())
+                        .orElseThrow(() -> new RuntimeException("Payment method not found."));
+                if (!pm.getUser().equals(payment.getOrder().getUser())) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Payment was nor found.");
+                } else {
+                    Long paymentMethodId = paymentDTO.paymentMethodId();
+                    System.out.println("4");
+                    PaymentMethod paymentMethod = paymentMethodRepository.findById(paymentMethodId).orElseThrow(() ->
+                            new RuntimeException("Payment method not found."));
+                    payment.setPaymentMethod(paymentMethod);
+                }
+            }
+
+            System.out.println("5");
+            payment.setStatus("PAID");
+            order.setStatus("CONFIRMED");
+            order.getPackageDetails().setStatus("PICKING_UP");
+
+            orderRepository.save(order);
+            paymentRepository.save(payment);
+
+
+            return ResponseEntity.ok("Payment made successfully.");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error occurred during payment.");
+        }
     }
 }
