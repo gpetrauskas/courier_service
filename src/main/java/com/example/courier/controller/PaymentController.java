73fb5a2924a449fdc5d094a79254b1f402fbfddd
gpetrauskas <inconoir@gmail.com>
2024-05-16 17:39:09 +0300

    feat: add logger.
    add endpoint to pay for an order (processPayment to the payment service).

--- src/main/java/com/example/courier/controller/PaymentController.java
+++ src/main/java/com/example/courier/controller/PaymentController.java
@@ -7,6 +7,8 @@ import com.example.courier.dto.PaymentDetailsDTO;
 import com.example.courier.repository.OrderRepository;
 import com.example.courier.repository.PaymentRepository;
 import com.example.courier.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ import java.security.Principal;
 @RequestMapping("/api/payment")
 public class PaymentController {
 
+    private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
     @Autowired
     private PaymentService paymentService;
     @Autowired
@@ -28,11 +31,30 @@ public class PaymentController {
     @Autowired
     private PaymentRepository paymentRepository;
 
+
+    @PostMapping("/pay/{paymentId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> makePayment(@PathVariable Long paymentId, @RequestBody PaymentDTO paymentDTO, Principal principal) {
+        Payment payment = paymentRepository.findById(paymentId).orElseThrow(() ->
+                new RuntimeException("payment not found."));
+
+        if (!principal.getName().equals(payment.getOrder().getUser().getEmail())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No payment with such id for this user.");
+        }
+        
+        try {
+            ResponseEntity response = paymentService.processPayment(paymentDTO, payment);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
     @PostMapping("/processPayment")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> processPayment(@RequestBody PaymentDTO paymentDTO) {
         try {
-            paymentService.processPayment(paymentDTO);
+          //  paymentService.processPayment(paymentDTO);
             return ResponseEntity.ok("Payment processed successfully.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Problem occurred during payment processing");
