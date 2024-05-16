cd9caffd12610e28ab576937f80df5ba4c750ad6
gpetrauskas <inconoir@gmail.com>
2024-05-17 00:01:51 +0300

    feat: add cvc field

--- src/main/java/com/example/courier/dto/PaymentDTO.java
+++ src/main/java/com/example/courier/dto/PaymentDTO.java
@@ -3,5 +3,5 @@ package com.example.courier.dto;
 import java.math.BigDecimal;
 
 public record PaymentDTO(OrderDTO orderDTO, Long paymentMethodId, PaymentMethodDTO newPaymentMethod,
-                         BigDecimal amount, String status) {
+                         BigDecimal amount, String status, String cvc) {
 }
