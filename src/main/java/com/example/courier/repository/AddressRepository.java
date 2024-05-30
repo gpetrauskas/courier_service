5ed4b80a64470f57510120c79ee44d4010d1ca46
gpetrauskas <inconoir@gmail.com>
2024-05-30 19:22:40 +0300

    Added AddressRepository

--- src/main/java/com/example/courier/repository/AddressRepository.java
+++ src/main/java/com/example/courier/repository/AddressRepository.java
@@ -1,4 +1,8 @@
 package com.example.courier.repository;
 
-public interface AddressRepository {
+import com.example.courier.domain.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
