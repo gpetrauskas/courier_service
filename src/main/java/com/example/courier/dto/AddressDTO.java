f37f7f935150c9104d87b5c1d82b4fe3151e6325
gpetrauskas <inconoir@gmail.com>
2024-05-30 19:15:26 +0300

    Added AddressDTO record. Imoemented fromAddress method to convert Address entity to AddressDTO and reverse toAddress method to convert AddressDTO to Address entity.

--- src/main/java/com/example/courier/dto/AddressDTO.java
+++ src/main/java/com/example/courier/dto/AddressDTO.java
@@ -1,4 +1,32 @@
 package com.example.courier.dto;
 
-public record AddressDTO() {
+import com.example.courier.domain.Address;
+
+public record AddressDTO(String city, String street, String houseNumber, String flatNumber, String phoneNumber,
+                         String postCode, String name) {
+
+    public static AddressDTO fromAddress(Address address) {
+        return new AddressDTO(
+                address.getCity(),
+                address.getStreet(),
+                address.getHouseNumber(),
+                address.getFlatNumber(),
+                address.getPhoneNumber(),
+                address.getPostCode(),
+                address.getName()
+        );
+    }
+
+    public static Address toAddress(AddressDTO addressDTO) {
+        Address address = new Address();
+        address.setCity(addressDTO.city());
+        address.setStreet(addressDTO.street());
+        address.setHouseNumber(addressDTO.houseNumber());
+        address.setFlatNumber(addressDTO.flatNumber());
+        address.setPhoneNumber(addressDTO.phoneNumber());
+        address.setPostCode(addressDTO.postCode());
+        address.setName(addressDTO.name());
+
+        return address;
+    }
 }
