31f3eb7953b652854ec5e7facd07015bae10cccc
gpetrauskas <inconoir@gmail.com>
2024-05-30 19:42:24 +0300

    Create CourierService (initial) for handling courier operations. Implemented getAllPakcagesToPickUp method to fetch packages with status PICKING_UP from package repository. Converts package entoties to PackageDTO

--- src/main/java/com/example/courier/service/CourierService.java
+++ src/main/java/com/example/courier/service/CourierService.java
@@ -1,4 +1,34 @@
 package com.example.courier.service;
 
+import com.example.courier.common.PackageStatus;
+import com.example.courier.domain.Package;
+import com.example.courier.dto.PackageDTO;
+//import com.example.courier.repository.CourierRepository;
+import com.example.courier.repository.PackageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CourierService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CourierService.class);
+
+    @Autowired
+    private PackageRepository packageRepository;
+   // @Autowired
+  //  private CourierRepository courierRepository;
+
+
+    public List<PackageDTO> getAllPackagesToPickUp() {
+        List<Package> packageList = packageRepository.findByStatus(PackageStatus.PICKING_UP);
+        List<PackageDTO> packageDTOList = packageList.stream()
+                .map(PackageDTO::packageToDTO).toList();
+
+        return packageDTOList;
+    }
+
 }
