45d64cbc777c5a68c074d8aaa80d8c7f76e9b0f6
gpetrauskas <inconoir@gmail.com>
2024-05-30 19:24:05 +0300

    added method findByStatus to find packages by status.

--- src/main/java/com/example/courier/repository/PackageRepository.java
+++ src/main/java/com/example/courier/repository/PackageRepository.java
@@ -1,10 +1,13 @@
 package com.example.courier.repository;
 
+import com.example.courier.common.PackageStatus;
 import com.example.courier.domain.Package;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PackageRepository extends JpaRepository<Package, Long> {
     Optional<Package> findByTrackingNumber(String trackingNumber);
+    List<Package> findByStatus(PackageStatus status);
 }
