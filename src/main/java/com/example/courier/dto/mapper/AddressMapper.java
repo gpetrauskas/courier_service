c49678df530b29ec6f0da10841af7d139243da5a
gpetrauskas <inconoir@gmail.com>
2024-06-03 21:01:14 +0300

    feat: added @mapping to toAddress method to ignore ID field for more security

--- src/main/java/com/example/courier/dto/mapper/AddressMapper.java
+++ src/main/java/com/example/courier/dto/mapper/AddressMapper.java
@@ -3,6 +3,7 @@ package com.example.courier.dto.mapper;
 import com.example.courier.domain.Address;
 import com.example.courier.dto.AddressDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
@@ -12,6 +13,7 @@ public interface AddressMapper {
     AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
 
     AddressDTO toAddressDTO(Address address);
+    @Mapping(target = "id", ignore = true)
     Address toAddress(AddressDTO addressDTO);
     void updateAddressFromDTO(AddressDTO addressDTO, @MappingTarget Address address);
 }
