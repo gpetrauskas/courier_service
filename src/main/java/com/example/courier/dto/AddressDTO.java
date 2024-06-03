9a0250a1c51eafa1f1597c890a9ec01e0ed57159
gpetrauskas <inconoir@gmail.com>
2024-06-03 21:02:13 +0300

    feat: added id field into AddressDTO

--- src/main/java/com/example/courier/dto/AddressDTO.java
+++ src/main/java/com/example/courier/dto/AddressDTO.java
@@ -1,8 +1,6 @@
 package com.example.courier.dto;
 
-import com.example.courier.domain.Address;
-
-public record AddressDTO(String city, String street, String houseNumber, String flatNumber, String phoneNumber,
+public record AddressDTO(Long id, String city, String street, String houseNumber, String flatNumber, String phoneNumber,
                          String postCode, String name) {
 
 
