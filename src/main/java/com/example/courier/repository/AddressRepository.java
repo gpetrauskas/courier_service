70eac92132f77addd827c209fe00152184ac87ab
gpetrauskas <inconoir@gmail.com>
2024-06-03 16:16:52 +0300

    AddressRepository

--- src/main/java/com/example/courier/repository/AddressRepository.java
+++ src/main/java/com/example/courier/repository/AddressRepository.java
@@ -3,6 +3,8 @@ package com.example.courier.repository;
 import com.example.courier.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressRepository extends JpaRepository<Address, Long> {
+import java.util.List;
 
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findByUserId(Long id);
 }
