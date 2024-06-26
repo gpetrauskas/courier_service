3af702384811287c9d9ec73117fbd5b050791fed
gpetrauskas <inconoir@gmail.com>
2024-06-27 00:38:25 +0300

    repo edit

--- src/main/java/com/example/courier/dto/mapper/PaymentMethodMapper.java
+++ src/main/java/com/example/courier/dto/mapper/PaymentMethodMapper.java
@@ -1,4 +1,13 @@
 package com.example.courier.dto.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface PaymentMethodMapper {
+
+    PaymentMethodMapper INSTANCE = Mappers.getMapper(PaymentMethodMapper.class);
+
 }
--- src/main/java/com/example/courier/repository/PaymentMethodRepository.java
+++ src/main/java/com/example/courier/repository/PaymentMethodRepository.java
@@ -4,5 +4,8 @@ import com.example.courier.domain.PaymentMethod;
 import com.example.courier.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+    List<PaymentMethod> findByUserId(Long id);
 }
