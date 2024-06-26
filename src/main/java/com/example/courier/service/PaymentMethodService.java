87b41b01d633feeb9ce7bebda4fde76ec3de13da
gpetrauskas <inconoir@gmail.com>
2024-06-27 00:39:48 +0300

    feat: add getSavedPayemntMethods and convertToDTO methods in ayamentMethodService.
    It retrieves saved payemnt method and converts them to DTO objects. Before that it checks if payment is instanceof CreditCard, in case of that CreditCardDTO is created. (later more methods).

--- src/main/java/com/example/courier/service/PaymentMethodService.java
+++ src/main/java/com/example/courier/service/PaymentMethodService.java
@@ -1,6 +1,7 @@
 package com.example.courier.service;
 
 import com.example.courier.domain.CreditCard;
+import com.example.courier.domain.PaymentMethod;
 import com.example.courier.domain.User;
 import com.example.courier.dto.CreditCardDTO;
 import com.example.courier.dto.PaymentMethodDTO;
@@ -8,6 +9,8 @@ import com.example.courier.repository.PaymentMethodRepository;
 import com.example.courier.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PaymentMethodService {
@@ -27,5 +30,29 @@ public class PaymentMethodService {
             CreditCard card = creditCardService.setupCreditCard((CreditCardDTO) paymentMethodDTO, user);
             paymentMethodRepository.save(card);
         }
+    }
+
+    public List<PaymentMethodDTO> getSavedPaymentMethods(Long userId) {
+        List<PaymentMethod> paymentMethods = paymentMethodRepository.findByUserId(userId);
+
+        return paymentMethods.stream()
+                .map(this::covertToDTO)
+                .toList();
+    }
+
+    private PaymentMethodDTO covertToDTO(PaymentMethod paymentMethod) {
+        if (paymentMethod instanceof CreditCard) {
+            CreditCard card = (CreditCard) paymentMethod;
+            return new CreditCardDTO(
+                    card.getId(),
+                    card.maskCardNumber(),
+                    card.getExpiryDate(),
+                    card.getCardHolderName(),
+                    "",
+                    card.isSaved());
+        }
+
+        return null;
+
     }
 }
