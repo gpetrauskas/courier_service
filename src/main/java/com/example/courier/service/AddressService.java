1aae64f83faf954b7ec160a11f721e0e48af24ea
gpetrauskas <inconoir@gmail.com>
2024-06-30 23:08:32 +0300

    feat: add methods related to Address like getAddress, getAddress, saveAddress, createNewAddress

--- src/main/java/com/example/courier/service/AddressService.java
+++ src/main/java/com/example/courier/service/AddressService.java
@@ -1,8 +1,10 @@
 package com.example.courier.service;
 
 import com.example.courier.domain.Address;
+import com.example.courier.domain.User;
 import com.example.courier.dto.AddressDTO;
 import com.example.courier.dto.mapper.AddressMapper;
+import com.example.courier.exception.AddressNotFoundException;
 import com.example.courier.repository.AddressRepository;
 import com.example.courier.repository.UserRepository;
 import org.slf4j.Logger;
@@ -32,15 +34,45 @@ public class AddressService {
 
     @Transactional
     public AddressDTO updateAddress(Long id, AddressDTO addressDTO) {
-        Address existingAddress = addressRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Address not found."));
+        Address existingAddress = getAddressById(id);
 
         addressMapper.updateAddressFromDTO(addressDTO, existingAddress);
-        addressRepository.save(existingAddress);
+        saveAddress(existingAddress);
 
         logger.info("updated address with id: {}", id);
 
         return addressMapper.toAddressDTO(existingAddress);
     }
 
+    public Address getAddress(AddressDTO addressDTO, User user) {
+        if (addressDTO.id() != null) {
+            Address address = getAddressById(addressDTO.id());
+            validateAddressUser(address, user);
+            return address;
+        }
+
+        return createNewAddress(addressDTO, user);
+    }
+
+    private void validateAddressUser(Address address, User user) {
+        if (!address.getUser().getId().equals(user.getId())) {
+            throw new RuntimeException("Address does not belong to the user.");
+        }
+    }
+
+    @Transactional
+    private Address createNewAddress(AddressDTO addressDTO, User user) {
+        Address address = addressMapper.toAddress(addressDTO);
+        address.setUser(user);
+        return addressRepository.saveAndFlush(address);
+    }
+
+    public Address getAddressById(Long id) {
+        return addressRepository.findById(id)
+                .orElseThrow(() -> new AddressNotFoundException("Address not found."));
+    }
+
+    public void saveAddress(Address address) {
+        addressRepository.save(address);
+    }
 }
