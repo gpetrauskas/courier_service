086b33b1c1bde99611f0f2f3e4a5cce39c0d8f0b
gpetrauskas <inconoir@gmail.com>
2024-07-04 21:16:49 +0300

    Refactor makePayment method for simplified code.
    moved user authorization and payment processing to processPayment method in PaymentService
    
    Removed redundant Payment retrieval and logging from controller method.

--- src/main/java/com/example/courier/controller/AddressController.java
+++ src/main/java/com/example/courier/controller/AddressController.java
@@ -1,9 +1,8 @@
 package com.example.courier.controller;
 
-import com.example.courier.domain.Address;
 import com.example.courier.dto.AddressDTO;
-import com.example.courier.dto.mapper.AddressMapper;
-import com.example.courier.repository.AddressRepository;
+import com.example.courier.exception.AddressNotFoundException;
+import com.example.courier.exception.UserAddressMismatchException;
 import com.example.courier.service.AddressService;
 import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
@@ -13,8 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -27,59 +24,38 @@ public class AddressController {
     private Logger log = LoggerFactory.getLogger(AddressController.class);
     @Autowired
     private AddressService addressService;
-    @Autowired
-    private AddressRepository addressRepository;
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/myAddressList")
-    public ResponseEntity<List<AddressDTO>> getMyAddressList() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        List<Address> myList = addressService.getAllMyAddresses(authentication.getName());
-        List<AddressDTO> myAddressDTOs = myList.stream()
-                .map(AddressMapper.INSTANCE::toAddressDTO)
-                .toList();
-        return ResponseEntity.ok(myAddressDTOs);
+    public ResponseEntity<List<AddressDTO>> getMyAddressList(Principal principal) {
+        List<AddressDTO> myList = addressService.getAllMyAddresses(principal.getName());
+
+        return ResponseEntity.ok(myList);
     }
 
     @PatchMapping("/update/{id}")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> updateAddress(@PathVariable Long id, @RequestBody AddressDTO addressDTO, Principal principal) {
         try {
-            Address address = addressRepository.findById(id)
-                    .orElseThrow(() -> new EntityNotFoundException("Address not found"));
-
-            if (!principal.getName().equals(address.getUser().getEmail())) {
-                throw new AccessDeniedException("Invalid request");
-            }
-            if (!id.equals(addressDTO.id())) {
-                throw new IllegalArgumentException("Cannot update address");
-            }
-
-            AddressDTO updatedAddress = addressService.updateAddress(id, addressDTO);
+            AddressDTO updatedAddress = addressService.updateAddress(id, addressDTO, principal);
 
             return ResponseEntity.ok(updatedAddress);
-        } catch (EntityNotFoundException | AccessDeniedException | IllegalArgumentException e) {
+        } catch (AddressNotFoundException e) {
+            log.error("Address with id {} not found", id);
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred.");
+        } catch (UserAddressMismatchException e) {
+            log.error("Address with id {} does not belong to user with email {}", id, principal.getName());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error occurred.");
+        } catch (Exception e) {
+            log.error("Error updating address with id {}: {}", id, e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong.");
         }
     }
 
     @DeleteMapping("/remove/{id}")
     public ResponseEntity<String> removeAddress(@PathVariable Long id, Principal principal) {
         try {
-            Address address = addressRepository.findById(id)
-                    .orElseThrow(() -> new EntityNotFoundException("Address not found"));
-
-            if (!principal.getName().equals(address.getUser().getEmail())) {
-                throw new AccessDeniedException("Invalid request");
-            }
-
-            if (!id.equals(address.getId())) {
-                throw new IllegalArgumentException("Cannot delete this.");
-            }
-
-            addressRepository.deleteById(id);
+            addressService.deleteAddressById(id, principal);
 
             return ResponseEntity.ok().body("Address was successfully deleted.");
         } catch (EntityNotFoundException | AccessDeniedException | IllegalArgumentException e) {
