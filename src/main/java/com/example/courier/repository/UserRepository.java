b7ed3e96f3a4cecbfad5afe50771b6ee52a8c60c
gpetrauskas <inconoir@gmail.com>
2024-11-19 00:14:54 +0200

    add JpaSpecificationExecutor to UserRepository for dynamic query capabilities.

--- src/main/java/com/example/courier/repository/UserRepository.java
+++ src/main/java/com/example/courier/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.courier.repository;
 
 import com.example.courier.domain.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     User findByEmail(String email);
     Optional<User> findById(Long id);
+
 }
