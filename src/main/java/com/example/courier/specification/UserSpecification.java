35deb654cf87fb7f76c578ddee5397b104bf0b36
gpetrauskas <inconoir@gmail.com>
2024-11-19 00:18:55 +0200

    Add UserSpecification class for dynamic query building

--- src/main/java/com/example/courier/specification/UserSpecification.java
+++ src/main/java/com/example/courier/specification/UserSpecification.java
@@ -1,4 +1,23 @@
 package com.example.courier.specification;
 
+import com.example.courier.common.Role;
+import com.example.courier.domain.User;
+import org.springframework.data.jpa.domain.Specification;
+
 public class UserSpecification {
+    public static Specification<User> hasRole(String role) {
+        return (root, query, criteriaBuilder) ->
+                role != null && !role.isEmpty() && Role.isValidRole(role) ?
+                        criteriaBuilder.equal(root.get("role"), Role.valueOf(role.toUpperCase()))
+                        :
+                        criteriaBuilder.conjunction();
+    }
+
+    public static Specification<User> hasUserId(Long userId) {
+        return (root, query, criteriaBuilder) ->
+                userId != null ?
+                        criteriaBuilder.equal(root.get("id"), userId)
+                        :
+                        criteriaBuilder.conjunction();
+    }
 }
