a46f7c87bb20ae799663992c9f1856a5387abc15
gpetrauskas <inconoir@gmail.com>
2025-06-24 23:14:51 +0300

    feat: add @InternalUseOnly annotation for markign internal methods

--- src/main/java/com/example/courier/validation/shared/InternalUseOnly.java
+++ src/main/java/com/example/courier/validation/shared/InternalUseOnly.java
@@ -1,4 +1,11 @@
 package com.example.courier.validation.shared;
 
-public interface InternalUseOnly {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface InternalUseOnly {
 }
