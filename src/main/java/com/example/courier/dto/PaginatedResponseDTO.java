34e1a8ce2e9be7f3c673e624be5c3f7c62293092
gpetrauskas <inconoir@gmail.com>
2025-07-02 17:32:00 +0300

    feat(pagination): added generic mapping constructor to PaginatedResponseDTO
    allows transformation from Page<E> to List<T> using Function<E, T>
    
    simplifies oaginated response creation and code reuse

--- src/main/java/com/example/courier/dto/PaginatedResponseDTO.java
+++ src/main/java/com/example/courier/dto/PaginatedResponseDTO.java
@@ -1,6 +1,9 @@
 package com.example.courier.dto;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
+import java.util.function.Function;
 
 public record PaginatedResponseDTO<T>(
         List<T> data,
@@ -8,4 +11,9 @@ public record PaginatedResponseDTO<T>(
         long totalItems,
         int totalPages
 ) {
+    public <E> PaginatedResponseDTO(Page<E> page, Function<E, T> mapper) {
+        this(page.getContent().stream().map(mapper).toList(),
+                page.getNumber(), page.getTotalElements(), page.getTotalPages()
+        );
+    }
 }
