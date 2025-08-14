d00a89ffec190ab2d127cc3fe3b3cac8a97d05d8
gpetrauskas <inconoir@gmail.com>
2025-08-15 00:29:37 +0300

    feat: added new method in PaymentRepository to fetch Payment by order and user ids

--- src/main/java/com/example/courier/repository/PaymentRepository.java
+++ src/main/java/com/example/courier/repository/PaymentRepository.java
@@ -2,6 +2,8 @@ package com.example.courier.repository;
 
 import com.example.courier.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,8 @@ import java.util.Optional;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByOrderId(Long orderId);
     List<Payment> findAllByOrderIdIn(List<Long> orderId);
+    @Query("SELECT p FROM Payments p JOIN p.order o " +
+            "JOIN o.user u " +
+            "WHERE p.order.id = :orderId AND u.id = :userId")
+    Optional<Payment> findByOrderIdAndUserId(@Param("orderId") Long orderId, @Param("userId") Long userId);
 }
