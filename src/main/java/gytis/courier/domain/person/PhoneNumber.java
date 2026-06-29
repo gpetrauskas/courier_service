ee9e2b33b7a7f2040e19d1870ec425689e52cb28
gpetrauskas <inconoir@gmail.com>
2026-06-29 12:12:19 +0300

    Small changes in PhoneNumber value object: prevent saving with empty spaces by replacing isBlank with isEmpty

--- src/main/java/gytis/courier/domain/person/PhoneNumber.java
+++ src/main/java/gytis/courier/domain/person/PhoneNumber.java
@@ -2,7 +2,7 @@ package gytis.courier.domain.person;
 
 public record PhoneNumber(String number) {
     public PhoneNumber {
-        if (number != null && !number.isBlank()) {
+        if (number != null && !number.isEmpty()) {
             if (number.length() != 8) throw new IllegalArgumentException("Phone number must contain 8 digits");
             if (!number.chars().allMatch(Character::isDigit)) {
                 throw new IllegalArgumentException("Phone number must be only digits");
